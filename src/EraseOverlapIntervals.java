63cc9b0ad607b5b8fa5844d33daca42492bf56f0
sherrrylin <1003342168@qq.com>
2019-01-20 22:09:03 +0800

    the sixth commit

---
+++ src/EraseOverlapIntervals.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+/*
+435. 无重叠区间
+给定一个区间的集合，找到需要移除区间的最小数量，使剩余区间互不重叠。
+
+注意:
+
+可以认为区间的终点总是大于它的起点。
+区间 [1,2] 和 [2,3] 的边界相互“接触”，但没有相互重叠。
+示例 1:
+
+输入: [ [1,2], [2,3], [3,4], [1,3] ]
+
+输出: 1
+
+解释: 移除 [1,3] 后，剩下的区间没有重叠。
+先计算最多能组成的不重叠区间个数，然后用区间总个数减去不重叠区间的个数。
+
+在每次选择中，区间的结尾最为重要，选择的区间结尾越小，留给后面的区间的空间越大，那么后面能够选择的区间个数也就越大。
+
+按区间的结尾进行排序，每次选择结尾最小，并且和前一个区间不重叠的区间。
+ */
+public class EraseOverlapIntervals {
+    public class Interval {
+        int start;
+        int end;
+
+        Interval() {
+            start = 0;
+            end = 0;
+        }
+
+        Interval(int s, int e) {
+            start = s;
+            end = e;
+        }
+    }
+
+    public int eraseOverlapIntervals(Interval[] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
+        Arrays.sort(intervals, new Comparator<Interval>() {
+            @Override
+            public int compare(Interval o1, Interval o2) {
+                return o1.end - o2.end;
+            }
+        });
+        int cnt = 1;
+        int end = intervals[0].end;
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i].start < end) {
+                continue;
+            }
+            end = intervals[i].end;
+            cnt++;
+        }
+        return intervals.length - cnt;
+
+    }
+}
