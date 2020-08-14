afc3c0481e416f647c9d4852da40904195fcbf8c
lin.shi <lin.shi@tendcloud.com>
2020-08-14 18:33:41 +0800

    136. 只出现一次的数字

---
+++ src/OnlyOneTimesNum.java
@@ -0,0 +1,22 @@
+public class OnlyOneTimesNum {
+
+
+    /**
+     * 136. 只出现一次的数字
+     *
+     * @param nums
+     * @return
+     */
+
+    public int singleNumber(int[] nums) {
+
+        int single = 0;
+
+        for (int num : nums) {
+            single ^= num;
+        }
+
+        return single;
+
+    }
+}
