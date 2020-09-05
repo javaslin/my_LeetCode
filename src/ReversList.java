672ce92e1fa402526f0fcdc35a8340ecc6e23311
lin.shi <lin.shi@tendcloud.com>
2020-09-05 18:57:23 +0800

    二分查找 列表有环 反转链表

---
+++ src/ReversList.java
@@ -0,0 +1,21 @@
+public class ReversList {
+
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode tmp = null;
+        while (cur != null) {
+            tmp = cur.next;
+
+            cur.next = pre;
+
+            pre = cur;
+
+            cur = tmp;
+        }
+        return pre;
+
+
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,33 @@
 public class Test {
 
 
+    private static Student student;
+
+    private static Student student1;
+
+    private static Student student2;
+
+
+    static {
+        student = new Student("tom", "");
+
+        student1 = new Student("mac", "");
+
+        student2 = new Student("xiaohong", "");
+
+    }
+
+
     public static void main(String[] args) {
 
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+
+        for (int i = 0; i < 5; ++i) {
+            System.out.println(i);
+        }
 
     }
 }
